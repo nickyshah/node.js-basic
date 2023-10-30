2ae9dc922dfc8b0cd05e59628c51e91ea74952f9
Nicky <nickshah534@gmail.com>
2023-10-30 21:23:43 +1100

    not to do list and api and database is connected

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { connectMongo } from './src/config/dbConfig.js';
+import {deleteManyTask, getAllTasks, insertTask, switchTask} from './src/model/TaskModel.js'
 
 
 
@@ -8,34 +10,73 @@ const PORT = 8000
 app.use(express.json())
 
 let fakeDb = [];
+connectMongo()
 
-app.get("/api/v1/task", (req, res)=>{
+app.get("/api/v1/task", async(req, res)=>{
+    const taskList = await getAllTasks()
     res.json({
         status: "Success",
-        message: "To-Do Soon",
-        fakeDb,
+        message: "Here are the TaskList",
+        taskList
+        
     })
 })
 
-app.post("/api/v1/task", (req, res)=>{
+app.post("/api/v1/task", async (req, res)=>{
+    const result = await insertTask(req.body)
     // console.log(req.body)
     fakeDb.push(req.body)
-    res.json({
+    result?._id 
+    ? res.json({
         status: "Success",
-        message: "To-Do Soon post"
+        message: "New task has been added!",
+        result
+    })
+    : res.json({
+        status: "error",
+        message: "Error, Unable to add the task, try again later",
+        result
     })
 })
 
-app.patch("/api/v1/task", (req, res)=>{
-    res.json({
+app.patch("/api/v1/task", async (req, res)=>{
+    // console.log(req.body)
+    const {_id, type} = req.body;
+    const result = await switchTask(_id, {type})
+    console.log(req.body, result)
+    result?._id 
+    ? res.json({
         status: "Success",
-        message: "To-Do Soon patch"
+        message: "The task has been updated!",
+    
+    })
+    : res.json({
+        status: "error",
+        message: "Error, Unable to update the task, try again later",
+    
     })
 })
-app.delete("/api/v1/task", (req, res)=>{
-    res.json({
+app.delete("/api/v1/task/", async (req, res)=>{
+    // const {_id} = req.params; 
+    // console.log(req.body)
+    const {ids} = req.body
+    console.log(ids)
+    const result = await deleteManyTask(ids)
+    // res.json({
+    //     status: "Success",
+    //     message: "To-Do Soon delete"
+    // })
+    console.log(result)
+    result?.deletedCount
+    ? res.json({
         status: "Success",
-        message: "To-Do Soon delete"
+        message: "All the selected task has been deleted successfully",
+    
+    })
+    : res.json({
+        status: "error",
+        message: "Error, Unable to delete the task, try again later",
+    
     })
 })
 
